f205dab51b57c816386bfc46860318579274865d
Mark Wigglesworth <mark.wigglesworth@digi-nut.co.uk>
2015-06-23 11:21:46 +0100

    theme1 update.

--- theme1/index.php
+++ theme1/index.php
@@ -25,8 +25,10 @@
 	<section>
 		<div class="grid">
 			<div class="row">
-				<div class="u-6-12"></div>
-				<div class="u-6-12">
+				<div class="u-6-12 mobile-12-12">
+					<img src="/img/infograph1.png" width="100%"/>
+				</div>
+				<div class="u-6-12 mobile-12-12">
 					<h1>This is a title spanning a few lines</h1>
 					<p>Vestibulum placerat finibus massa, ac vestibulum nisl bibendum at. Aliquam velit risus, fermentum in ipsum id, sagittis rhoncus dui? Vivamus nulla felis, bibendum vel magna a, pulvinar tempus sem.</p>
 					<a class="button">Read more</a>
@@ -37,33 +39,79 @@
 	<section>
 		<div class="grid">
 			<div class="row">
-				<div class="u-4-12">
-					<span class="feature icon-facebook"></span>
+				<div class="u-4-12 mobile-12-12 feature">
+					<span class="icon-facebook"></span>
+					<h2>Title Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+					<a class="button">Read more</a>
 				</div>
-				<div class="u-4-12">2</div>
-				<div class="u-4-12">3</div>
+				<div class="u-4-12 mobile-12-12  feature">
+					<span class="icon-twitter"></span>
+					<h2>Title Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+					<a class="button">Read more</a>
+				</div>
+				<div class="u-4-12 mobile-12-12  feature">
+					<span class="icon-gplus"></span>
+					<h2>Title Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+					<a class="button">Read more</a>
+				</div>
 			</div>
 		</div>
 	</section>
 	<section>
 		<div class="grid">
 			<div class="row">
-				<div class="u-2-12 mobile-2-4">1</div>
-				<div class="u-4-12 mobile-2-4">2</div>
-				<div class="u-2-12 mobile-2-4">3</div>
-				<div class="u-4-12 mobile-2-4">4</div>
+				<div class="u-6-12 mobile-4-4">
+					<h2>Title Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+					<p>Integer tincidunt. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nam commodo suscipit quam. Morbi ac felis. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue.</p>
+					<a class="button">Read more</a><a class="button">get in touch</a>
+				</div>
+				<div class="u-6-12 mobile-4-4">
+					<img src="/img/infograph2.png" width="100%"/>
+				</div>
 			</div>
 		</div>
 	</section>
 	<section>
+
 		<div class="grid">
+			<h2 class="align-center">What We Do</h2>
 			<div class="row">
-				<div class="u-2-6 mobile-4-12"></div>
-				<div class="u-2-6 mobile-4-12"></div>
-				<div class="u-2-6 mobile-4-12"></div>
-				<div class="u-2-6 mobile-4-12"></div>
-				<div class="u-2-6 mobile-4-12"></div>
-				<div class="u-2-6 mobile-4-12"></div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-facebook">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-twitter">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-gplus">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-facebook">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-twitter">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-gplus">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-facebook">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
+				<div class="u-3-12 tablet-6-12 mobile-12-12 service">
+					<h3 class="icon-twitter">Titles Goes Here</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+				</div>
 			</div>
 		</div>
 	</section>
