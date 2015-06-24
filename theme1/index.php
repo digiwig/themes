67ec18f8e64d3b3376b1453501f731904a8d5579
Mark Wigglesworth <mark.wigglesworth@digi-nut.co.uk>
2015-06-24 08:36:17 +0100

    Adding footer and cookie policy.

--- theme1/index.php
+++ theme1/index.php
@@ -20,6 +20,7 @@
 	<![endif]-->		       
 </head>
 <body>
+<div id="cookies"><p>Cookies on the wesbite: This website uses cookies. Click here to close.</p></div>
 <header></header>
 <main>
 	<section>
@@ -117,6 +118,7 @@
 	</section>
 	<section>
 		<div class="grid">
+			<h2 class="align-center">The Team</h2>
 			<div class="row social">
 				<div class="mobile-4-12">
 					<a href="#" class="icon-facebook" ><img src="/img/square.png" width="100%"/></a>
@@ -143,12 +145,48 @@
 <footer>
 	<div class="grid">
 		<div class="row">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+			<div class="u-3-12 mobile-6-12">
+				<h4>Contact</h4>
+				<address>
+					<a href="mailto:exampe@example.com">E: example@example.com
+					<a href="tel:+18501234567">T: 850-123-4567</a>
+					<span>12 Queen Street<br/>Soho Gardens<br/>London<br/>W2 6PR</span>
+				</address>
+			</div>
+			<div class="u-3-12 mobile-6-12">
+				<h4>Quick Links</h4>
+				<menu>
+					<a href="">Link A</a>
+					<a href="">Link B</a>
+					<a href="">Link C</a>
+					<a href="">Link D</a>
+				</menu>
+				<h4>Social</h4>
+				<menu class="icons">
+					<a class="icon-facebook"></a>
+					<a class="icon-twitter"></a>
+					<a class="icon-pinterest"></a>
+					<a class="icon-vimeo"></a>
+				</menu>
+			</div>
+			<div class="u-6-12 mobile-12-12">
+				<h4>Newsletter</h4>
+				<p>Subscribe to our newsletter and keep up to date with the latest news and events</p>
+				<form>
+					<input type="text"/>
+					<input type="submit" value="Sign up"/>
+				</form>
+			</div>
 		</div>
-		<p>&copy; Copyright 2015. Diginut Ltd.</p>
+		<div class="row legals">
+			<span class="u-6-12 mobile-12-12">&copy; Copyright 2015. Diginut Ltd.</span>
+			<menu class="u-6-12 mobile-12-12 links">
+				<a href="">Terms of Use</a>
+				<a href="">Privacy Policy</a>
+				<a href="">Cookies</a>
+			</menu>
+		</div>
+		
 	</div>
 </footer>
 
@@ -177,7 +215,9 @@
 		if('ontouchstart' in window || navigator.msMaxTouchPoints){
 			getScript("/js/vendor/fastclick.js");			
 		}	
-		getScript("/js/scripts.min.js");
+		getScript("/js/scripts.min.js", function() {
+			getScript("/js/plugins/cookies.js");
+		});
 	});
 })();
 </script>
