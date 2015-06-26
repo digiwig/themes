802f4f282743a21c569f75ef91bd836e9cf0bb3b
Mark Wigglesworth <mark.wigglesworth@digi-nut.co.uk>
2015-06-26 09:01:35 +0100

    Adding nav and a demo logo.

--- theme1/index.php
+++ theme1/index.php
@@ -21,7 +21,20 @@
 </head>
 <body>
 <div id="cookies"><p>Cookies on the wesbite: This website uses cookies. Click here to close.</p></div>
-<header></header>
+<header>
+
+	<nav>
+		<a><img src="/img/logo_demo.png" width="100%"/></a>
+		<menu>
+			<a href="#" class="active">Home</a>
+			<a href="#">About Us</a>
+			<a href="#">Services</a>
+			<a href="#">Blog</a>
+			<a href="#">Contact</a>
+		</menu>
+	</nav>
+	<button></button>
+</header>
 <main>
 	<section>
 		<div class="grid">
@@ -141,56 +154,54 @@
 			</div>
 		</div>
 	</section>
+	<footer>
+		<div class="grid">
+			<div class="row">
+				<div class="u-3-12 mobile-7-12">
+					<h4>Contact</h4>
+					<address>
+						<a href="mailto:exampe@example.com">E: example@example.com
+						<a href="tel:+18501234567">T: 850-123-4567</a>
+						<span>12 Queen Street<br/>Soho Gardens<br/>London<br/>W2 6PR</span>
+					</address>
+				</div>
+				<div class="u-3-12 mobile-5-12">
+					<h4>Quick Links</h4>
+					<menu>
+						<a href="">Link A</a>
+						<a href="">Link B</a>
+						<a href="">Link C</a>
+						<a href="">Link D</a>
+					</menu>
+					<h4>Social</h4>
+					<menu class="icons">
+						<a class="icon-facebook"></a>
+						<a class="icon-twitter"></a>
+						<a class="icon-pinterest"></a>
+						<a class="icon-vimeo"></a>
+					</menu>
+				</div>
+				<div class="u-6-12 mobile-12-12">
+					<h4>Newsletter</h4>
+					<p>Subscribe to our newsletter and keep up to date with the latest news and events</p>
+					<form>
+						<input type="text"/>
+						<input type="submit" value="Sign up"/>
+					</form>
+				</div>
+			</div>
+			<div class="row legals">
+				<span class="u-6-12 mobile-12-12">&copy; Copyright 2015. Diginut Ltd.</span>
+				<menu class="u-6-12 mobile-12-12 links">
+					<a href="">Terms of Use</a>
+					<a href="">Privacy Policy</a>
+					<a href="">Cookies</a>
+				</menu>
+			</div>
+			
+		</div>
+	</footer>
 </main>
-<footer>
-	<div class="grid">
-		<div class="row">
-			<div class="u-3-12 mobile-6-12">
-				<h4>Contact</h4>
-				<address>
-					<a href="mailto:exampe@example.com">E: example@example.com
-					<a href="tel:+18501234567">T: 850-123-4567</a>
-					<span>12 Queen Street<br/>Soho Gardens<br/>London<br/>W2 6PR</span>
-				</address>
-			</div>
-			<div class="u-3-12 mobile-6-12">
-				<h4>Quick Links</h4>
-				<menu>
-					<a href="">Link A</a>
-					<a href="">Link B</a>
-					<a href="">Link C</a>
-					<a href="">Link D</a>
-				</menu>
-				<h4>Social</h4>
-				<menu class="icons">
-					<a class="icon-facebook"></a>
-					<a class="icon-twitter"></a>
-					<a class="icon-pinterest"></a>
-					<a class="icon-vimeo"></a>
-				</menu>
-			</div>
-			<div class="u-6-12 mobile-12-12">
-				<h4>Newsletter</h4>
-				<p>Subscribe to our newsletter and keep up to date with the latest news and events</p>
-				<form>
-					<input type="text"/>
-					<input type="submit" value="Sign up"/>
-				</form>
-			</div>
-		</div>
-		<div class="row legals">
-			<span class="u-6-12 mobile-12-12">&copy; Copyright 2015. Diginut Ltd.</span>
-			<menu class="u-6-12 mobile-12-12 links">
-				<a href="">Terms of Use</a>
-				<a href="">Privacy Policy</a>
-				<a href="">Cookies</a>
-			</menu>
-		</div>
-		
-	</div>
-</footer>
-
-
 <!--[if !(IE 8)]><!-->
 <script type="text/javascript">
 (function() {
@@ -217,6 +228,7 @@
 		}	
 		getScript("/js/scripts.min.js", function() {
 			getScript("/js/plugins/cookies.js");
+			getScript("/js/plugins/navigation.js");
 		});
 	});
 })();
